4bcb627d6caec25a638cff3eb50656d87ec5e559
Woods <freelightingwmh@gmail.com>
2023-03-06 09:23:57 +0200

    Update README.md
    refine utils code logic to use split pdf function

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# signPDF
+# sign-pdf-with-transparent-background-signature
 
 Sign PDF file with signature photo, the tool extracts signature trace and merge it into PDF with specific page, postions and scale values. Besides it, the repo supports other PDF operation such as merge, OCR, watermark functions.
 
 ## Introduction
 
 The tool takes PDF and signature image inputs and output a signed PDF file.
+
 ![example result](./resource/introduction.png)
 
-In the picture above, you could see an example of the input photo and output result, the full signed PDF can be found from [files/example-pdf.pdf_signed.pdf](./files/example-pdf.pdf_signed.pdf):
+In the picture above, you could see an example of the input photo and output result, the full signed PDF can be found from [files/example-pdf_signed.pdf](./files/example-pdf_signed.pdf):
 
 
 ## Requirements
@@ -16,8 +17,35 @@ In Python 3.7 environment, install dependencies with `pip install -r requirement
 ## Usage
 
 Put the input files to `./files` folder, and `python main.py -h` to check arguments meaning.
+Here lists essential arguments for signing PDF:
+```python
+'-b', '--base-path', default='./files', type=str, help='Base path to the PDF files for processing'
+'-t', '--type-of-manipulation', required=True, type=str, \
+              choices=['ocr', 'merge', 'split', 'watermark', 'signature'], \
+              help="Type of PDF manipulation"
+'-i', '--input-files', required=True, nargs='+', help="Input PDF files name(s), add space between two files"
+'-s', '--signature-file', type=str, help="Sinature picture file name"
+'-n', '--signature-page-num', type=int, default=1, help="Signature page number [1, +Inf) of PDF file"
+'-o', '--signature-offset-xy', type=parse_two_numbers, default=[0,0], help="Offset of x and y coordinates of the signature"
+'-c', '--signature-scale', type=float, default=1, help="Scale (0,+inf) the input sgnature file, set it to negative value if need rotate signature"
+'-g', '--gray-threshold', type=float, default=100, help="Gray threshold [0,255] to process signature image, higher value, lower gray threshold"
+```
 
-Here are some usage examples assuming you have input files in the `./files` folder:
+### Signature
+
+- Take a signature photo such as the [example](./files/example-signature.jpg).
+- Put the signature photo and PDF file to `./files` folder.
+- Select the signature page (`-n`), gray threshold (`-g`), scale (`-c`), and offset positions (`-o`), the later two arguments might need try a few times to get best results.
+  - Page starts from 1.
+  - Gray threshold's range is [0, 255], it is used to extract signature from photo, the default value should be enough for most cases. If not, adjust it and check result. 
+  - Scale's range is [0, +inf), usually needs to decrease the signature size such as 0.3. Set it to negative value will rotate signature.
+  - Offset position is in X,Y oder, the top-left corner of the PDF page is the origin, Y is horizontal and X is vertical.
+- With above, run the program to sign signature. The examples signed [PDF](./files/example-pdf_signed.pdf) uses this command:
+`python main.py -t signature -i example-pdf.pdf -s example-signature.jpg -c 0.2 -n 2 -o 1310,1000`
+
+### Other functions
+
+Here are some other usage examples assuming you have input files in the `./files` folder:
 - OCR pdf and save to txt file
  `python main.py -t ocr -i a.pdf`
 - Merge mulitple PDFs into one PDF
@@ -26,11 +54,6 @@ Here are some usage examples assuming you have input files in the `./files` fold
 `python main.py -t watermark -i a.pdf -w watermark.pdf`;
   Add watermark to first (or last) page only
 `python main.py -t watermakr -i a.pdf -w watermark.pdf -p first`
-- Sign PDF file with input signature photo, examples can be found from `./files` folder.
-`python main.py -t signature -i example-pdf.pdf -s example-signature.jpg -n 1 -c 0.2 -g 100 -o 1280,1000`
-
 ## TODO
-- [ ] Create class for util codes.
-- [ ] Refine codes with logger information, better argument naming rule.
-- [ ] Use splitting PDF to PDFs function in signature function.
 - [ ] Add details guide to use the toolbox.
+- [ ] Add frontend webpage.
--- utils/pdfUtils.py
+++ utils/pdfUtils.py
@@ -1,105 +1,152 @@
 #!/usr/bin/python3
-import PyPDF2
 import os
-import utils.constant as ct
-# from reportlab.lib.pagesizes import letter
+# Image operation
+from PIL import Image
+import cv2
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from pdf2image import convert_from_path
-from PIL import Image
-import cv2
-import numpy as np
+# PDF operation
 import img2pdf
+import PyPDF2
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger
+from pdf2image import convert_from_path
+# Math
 from math import floor
+import numpy as np
+# Internal lib
+import utils.constant as ct
 
-def ocrAndSaveTxt(input_pdf):
-    pdfFile = open(input_pdf,'rb')
-    ct.logger.info('Loading file: %s' % os.path.basename(input_pdf))
-    pdfReader = PyPDF2.PdfReader(pdfFile)
-    ct.logger.info("Total pages are: %i" % len(pdfReader.pages))
-    with open(input_pdf + '.txt', 'w') as f:
-        for i in range (len(pdfReader.pages)):
-            page = pdfReader.pages[i]
-            f.write(page.extract_text())
-    pdfFile.close()
-    ct.logger.info("TXT file is saved to: %s" % (input_pdf + '.txt'))
+class pdfUtils:
+    def __init__(self, base_path, pdf_file_list):
+        self.file_name_list = pdf_file_list
+        self.file_name = os.path.basename(pdf_file_list[0])
+        self.file_name_without_extenstion = os.path.splitext(self.file_name)[0]
+        self.relative_path = base_path
+        self.absolute_path = os.path.abspath(self.relative_path)
+    
+    def ocrAndSaveTxt(self):
+        for i in range(len(self.file_name_list)):
+            with open(self.absolute_path + '/' + self.file_name_list[i], 'rb') as pdf:
+                pdf_reader = PdfReader(pdf)
+                saved_txt_path = \
+                    self.absolute_path + '/' + os.path.splitext(self.file_name_list[i])[0] + '.txt'
+                ct.logger.info('Loading file: %s' % self.file_name_list[i])
+                with open(saved_txt_path, 'w') as f:
+                    for j in range (len(pdf_reader.pages)):
+                        page = pdf_reader.pages[j]
+                        f.write(page.extract_text())
+                pdf.close()
+                ct.logger.info("TXT file is saved to: %s" % (saved_txt_path))
 
-def mergeFiles(input_pdfs, output_file):
-    pdfMerger = PyPDF2.PdfMerger()
-    for pdf in input_pdfs:
-        ct.logger.info('Loading file: %s' % os.path.basename(pdf))
-        with open(pdf,'rb') as f:
-            pdfMerger.append(f)
-    with open(output_file,'wb') as f:
-        pdfMerger.write(f)
-    ct.logger.info('Merged file saved: %s' % os.path.basename(output_file))
+    def mergePdfs(self):
+        pdf_merger = PdfMerger()
+        for i in range(len(self.file_name_list)):
+            ct.logger.info('Loading file: %s' % self.file_name_list[i])
+            with open(self.absolute_path + '/' + self.file_name_list[i], 'rb') as pdf:
+                pdf_merger.append(pdf)
+        output_path = self.absolute_path + '/' + self.file_name_without_extenstion + '_merged.pdf'
+        with open(output_path,'wb') as f:
+            pdf_merger.write(f)
+        ct.logger.info('Merged file is saved to: %s' % output_path)
 
-def watermark(input_pdf, input_watermark, add_to_page):
-    pdfOut = PyPDF2.PdfWriter()
-    ct.logger.info('Loading pdf file: %s' % os.path.basename(input_pdf))
-    pdfFile = open(input_pdf, 'rb')
-    pdfReader = PyPDF2.PdfReader(pdfFile)
-    ct.logger.info('Loading watermark file: %s' % os.path.basename(input_watermark))
-    pdfWatermark = PyPDF2.PdfReader(open(input_watermark, 'rb'), strict=False)
-    ct.logger.info('Adding watermark to %s page(s)' % add_to_page)
-    for i in range(len(pdfReader.pages)):
-        page = pdfReader.pages[i]
-        if add_to_page == 'all':
-            page.merge_page(pdfWatermark.pages[0])
-        elif add_to_page == 'first':
-            if i == 0:
-                page.merge_page(pdfWatermark.pages[0])
-        elif add_to_page == 'last':
-            if i == len(pdfReader.pages) - 1:
-                page.merge_page(pdfWatermark.pages[0])
-        page.compress_content_streams()
-        pdfOut.add_page(page)
-    pdfOut.write(open(input_pdf + "_watermark.pdf", 'wb'))
-    ct.logger.info('Watermarked file saved to: %s' % os.path.basename(input_pdf + "_watermark.pdf"))
+    def splitPdf(self):
+        with open(self.absolute_path + '/' + self.file_name_list[0], 'rb') as f:
+            pdf_reader = PdfReader(f)
+            # Iterate over each page and create a new PDF for each page
+            for page_num in range(len(pdf_reader.pages)):
+                writer = PdfWriter()
+                writer.add_page(pdf_reader.pages[page_num])
+                # Write the new PDF file
+                output_path = self.absolute_path + '/' + \
+                    self.file_name_without_extenstion + '_page_' + str(page_num) + '.pdf'
+                with open(output_path, 'wb') as out:
+                    writer.write(out)
+        ct.logger.info('Split files are saved to: %s' % self.absolute_path)
 
-def signature(input_pdf, input_signature, page, offset_xy, scale, gray_threshold):
-    fileName = os.path.basename(input_pdf)
-    fileNameWithoutExtenstion = os.path.splitext(fileName)[0]
-    images = convert_from_path(input_pdf)
-    pathRela = os.path.dirname(input_pdf)
-    pathAbs = os.path.abspath(pathRela)
-    pathTemp = pathAbs + "/temp"
-    isExist = os.path.exists(pathTemp)
-    if not isExist:
-        os.makedirs(pathTemp)
-    for i in range(len(images)):
-        images[i].save(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(i) + '.jpg', 'JPEG')
+    def watermark(self, input_watermark, add_to_page):
+        pdf_out = PdfWriter()
+        ct.logger.info('Loading pdf file: %s' % self.file_name_list[0])
+        with open(self.absolute_path + '/' + self.file_name_list[0], 'rb') as f:
+            pdf_reader = PdfReader(f)
+            ct.logger.info('Loading watermark file: %s' % os.path.basename(input_watermark))
+            watermark = PdfReader(open(self.absolute_path + '/' + input_watermark, 'rb'), strict=False)
 
-    pdfPage = cv2.imread(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(page) + '.jpg') 
-    signature = cv2.imread(input_signature)
-    if signature.shape[0] > pdfPage.shape[0] or signature.shape[1] > pdfPage.shape[1]:
-         scale_ = floor(min(pdfPage.shape[0]/signature.shape[0], pdfPage.shape[1]/signature.shape[1]))
-         if scale > scale_: scale = scale_
-    signatureGray = cv2.cvtColor(signature, cv2.COLOR_BGR2GRAY)
-    signatureCoords = np.column_stack(np.where(signatureGray < gray_threshold))
-    signatureCoords = signatureCoords * scale
-    signatureCoords = signatureCoords + offset_xy
-    for coord in signatureCoords:
-        pdfPage[int(coord[0]), int(coord[1])] = 0
-    cv2.imwrite(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(page) + '.jpg', pdfPage)
+            for i in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[i]
+                if add_to_page == 'all':
+                    page.merge_page(watermark.pages[0])
+                elif add_to_page == 'first':
+                    if i == 0:
+                        page.merge_page(watermark.pages[0])
+                elif add_to_page == 'last':
+                    if i == len(pdf_reader.pages) - 1:
+                        page.merge_page(watermark.pages[0])
+                page.compress_content_streams()
+                pdf_out.add_page(page)
+            output_path = self.absolute_path + '/' + \
+                    self.file_name_without_extenstion + '_watermark.pdf'
+            with open(output_path, 'wb') as out:
+                pdf_out.write(out)
+            ct.logger.info('Watermarked file is saved to: %s' % output_path)
 
-    for image in range(len(images)):
-        image_width, image_height = images[image].size
-        c = canvas.Canvas(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(image) + '.pdf' , pagesize=(image_width, image_height))
-        c.drawImage(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(image) + '.jpg', 0, 0, image_width, image_height)
-        os.remove(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(image) + '.jpg')
+    def signature(self, input_signature, page, offset_xy, scale, gray_threshold):
+        temp_path = self.absolute_path + "/temp"
+        isExist = os.path.exists(temp_path)
+        if not os.path.exists(temp_path):
+            os.makedirs(temp_path)
+        pdf_width = 0
+        pdf_height = 0
+        # Split PDF to multiple ones
+        with open(self.absolute_path + '/' + self.file_name_list[0], 'rb') as f:
+            pdf_reader = PdfReader(f)
+            page_width = pdf_reader.pages[0].mediabox.width
+            page_height = pdf_reader.pages[0].mediabox.height
+            # Iterate over each page and create a new PDF for each page
+            for page_num in range(len(pdf_reader.pages)):
+                writer = PdfWriter()
+                writer.add_page(pdf_reader.pages[page_num])
+                # Write the new PDF file
+                output_filename = temp_path + '/' + self.file_name_without_extenstion + '_page_' + str(page_num) + '.pdf'
+                with open(output_filename, 'wb') as out:
+                    writer.write(out)
+        # Image processing
+        page_to_be_signed = temp_path + '/' + self.file_name_without_extenstion + '_page_' + str(page-1)
+        images = convert_from_path(page_to_be_signed + '.pdf')
+        images[0].save(page_to_be_signed + '.jpg', 'JPEG')
+        os.remove(page_to_be_signed + '.pdf')
+        cv_page = cv2.imread(page_to_be_signed + '.jpg')
+        cv_signature = cv2.imread(self.absolute_path + '/' + input_signature)
+        # Size check
+        if cv_signature.shape[0] > cv_page.shape[0] or cv_signature.shape[1] > cv_page.shape[1]:
+            scale_min = floor(min(cv_page.shape[0]/cv_signature.shape[0], cv_page.shape[1]/cv_signature.shape[1]))
+            if scale > scale_min: scale = scale_min
+        signature_gray = cv2.cvtColor(cv_signature, cv2.COLOR_BGR2GRAY)
+        signature_coords = np.column_stack(np.where(signature_gray < gray_threshold))
+        signature_coords = signature_coords * scale
+        signature_coords = signature_coords + offset_xy
+        # Plot signature to cv page
+        for coord in signature_coords:
+            cv_page[int(coord[0]), int(coord[1])] = 0
+        cv2.imwrite(page_to_be_signed + '.jpg', cv_page)
+        # Convert cv page to PDF page
+        c = canvas.Canvas(page_to_be_signed + '.pdf' , pagesize=(page_width, page_height))
+        c.drawImage(page_to_be_signed + '.jpg', 0, 0, page_width, page_height)
+        os.remove(page_to_be_signed + '.jpg')
         c.save()
-    output_pdf = PyPDF2.PdfWriter()
-    for i in range(len(images)):
-        pdf_file = open(pathAbs + '/temp/' + fileNameWithoutExtenstion + '_page' + str(i) + '.pdf', 'rb')
-        input_pdf_reader = PyPDF2.PdfReader(pdf_file)
-        for page in range(len(input_pdf_reader.pages)):
-            output_pdf.add_page(input_pdf_reader.pages[page])
-    with open(input_pdf + '_signed.pdf', 'wb') as output_file:
-        output_pdf.write(output_file)
-    for file_name in os.listdir(pathAbs + '/temp/'):
-        file_path = os.path.join(pathAbs + '/temp/', file_name)
-        os.remove(file_path)
-    os.rmdir(pathAbs + '/temp/')
-
+        # Merge PDFs to PDF and save
+        output_pdf = PyPDF2.PdfWriter()
+        for i in range(len(pdf_reader.pages)):
+            pdf_file = open(temp_path + '/' + self.file_name_without_extenstion + '_page_' + str(i) + '.pdf', 'rb')
+            input_pdf_reader = PdfReader(pdf_file)
+            for page in range(len(input_pdf_reader.pages)):
+                output_pdf.add_page(input_pdf_reader.pages[page])
+        output_path = self.absolute_path + '/' + \
+                    self.file_name_without_extenstion + '_signed.pdf'
+        with open(output_path, 'wb') as output_file:
+            output_pdf.write(output_file)
+        for file_name in os.listdir(temp_path):
+            file_path = os.path.join(temp_path, file_name)
+            os.remove(file_path)
+        os.rmdir(temp_path)
+        ct.logger.info('Signed file is saved to: %s' % output_path)
